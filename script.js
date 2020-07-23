6cd5411a5eb5760de131ec5730419a98f90a799d
spencermdavis <spencermdavis@mac.com>
2020-07-23 18:29:29 -0400

    styling polish, moved buttons from corners to center of div, added background color change at 0 life :heavy_dollar_sign:

--- script.js
+++ script.js
@@ -25,21 +25,37 @@ trackerApp.incrementDecrement = () => {
     let lifeTotal = parseInt($(".p1Life").html());
     lifeTotal--;
     $(".p1Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p1Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player2 .decrement1").on("click", function () {
     let lifeTotal = parseInt($(".p2Life").html());
     lifeTotal--;
     $(".p2Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p2Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player3 .decrement1").on("click", function () {
     let lifeTotal = parseInt($(".p3Life").html());
     lifeTotal--;
     $(".p3Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p3Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player4 .decrement1").on("click", function () {
     let lifeTotal = parseInt($(".p4Life").html());
     lifeTotal--;
     $(".p4Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p4Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player1 .increment5").on("click", function () {
     let lifeTotal = parseInt($(".p1Life").html());
@@ -65,21 +81,37 @@ trackerApp.incrementDecrement = () => {
     let lifeTotal = parseInt($(".p1Life").html());
     lifeTotal = lifeTotal - 5;
     $(".p1Life").text(lifeTotal);
+    if (parseInt($(".p1Life").html()) <= 0) {
+      $(".p1Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player2 .decrement5").on("click", function () {
     let lifeTotal = parseInt($(".p2Life").html());
     lifeTotal = lifeTotal - 5;
     $(".p2Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p2Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player3 .decrement5").on("click", function () {
     let lifeTotal = parseInt($(".p3Life").html());
     lifeTotal = lifeTotal - 5;
     $(".p3Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p3Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player4 .decrement5").on("click", function () {
     let lifeTotal = parseInt($(".p4Life").html());
     lifeTotal = lifeTotal - 5;
     $(".p4Life").text(lifeTotal);
+    if (lifetotal <= 0) {
+      $(".p4Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player1 .increment10").on("click", function () {
     let lifeTotal = parseInt($(".p1Life").html());
@@ -105,23 +137,52 @@ trackerApp.incrementDecrement = () => {
     let lifeTotal = parseInt($(".p1Life").html());
     lifeTotal = lifeTotal - 10;
     $(".p1Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p1Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player2 .decrement10").on("click", function () {
     let lifeTotal = parseInt($(".p2Life").html());
     lifeTotal = lifeTotal - 10;
     $(".p2Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p2Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player3 .decrement10").on("click", function () {
     let lifeTotal = parseInt($(".p3Life").html());
     lifeTotal = lifeTotal - 10;
     $(".p3Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p3Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
   $(".player4 .decrement10").on("click", function () {
     let lifeTotal = parseInt($(".p4Life").html());
     lifeTotal = lifeTotal - 10;
     $(".p4Life").text(lifeTotal);
+    if (lifeTotal <= 0) {
+      $(".p4Life").css("background-color", "#A4243B");
+      $(".p1Life").css("border", "1px solid #eae0c2");
+    }
   });
 };
+
+if (parseInt($(".p1Life").html()) <= 0) {
+  $(".p1Life").css("background-color", "tomato");
+}
+if (parseInt($(".p2Life").html()) <= 0) {
+  $(".p2Life").css("background-color", "tomato");
+}
+if (parseInt($(".p3Life").html()) <= 0) {
+  $(".p3Life").css("background-color", "tomato");
+}
+if (parseInt($(".p4Life").html()) <= 0) {
+  $(".p4Life").css("background-color", "tomato");
+}
 
 trackerApp.displaySetter = () => {
   // prevent users from entering empty string
@@ -170,6 +231,8 @@ trackerApp.displaySetter = () => {
     }
   });
 };
+
+trackerApp.lifeAlert = () => {};
 
 trackerApp.init = () => {
   trackerApp.incrementDecrement();
