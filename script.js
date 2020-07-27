4f8538114e89b70a03d9c6648e1dda1a81981214
spencermdavis <spencermdavis@mac.com>
2020-07-27 01:30:27 -0400

    button listener refactor, form->layout factor, grid responsiveness refactor :heavy_dollar_sign:

--- script.js
+++ script.js
@@ -1,252 +1,57 @@
 const trackerApp = {};
 
+// listen for click on each module's buttons
+// on click, parse button value and update appropriate life span
+// toggle background to red when life = 0 to indicate game loss
 trackerApp.incrementDecrement = () => {
-  $(".player1 .increment1").on("click", function () {
+  $(".player1Buttons").on("click", function () {
     let lifeTotal = parseInt($(".p1Life").text());
-    lifeTotal++;
-    $(".p1Life").text(lifeTotal);
-  });
-  $(".player2 .increment1").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal++;
-    $(".p2Life").text(lifeTotal);
-  });
-  $(".player3 .increment1").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal++;
-    $(".p3Life").text(lifeTotal);
-  });
-  $(".player4 .increment1").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal++;
-    $(".p4Life").text(lifeTotal);
-  });
-  $(".player5 .increment1").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal++;
-    $(".p5Life").text(lifeTotal);
-  });
-  $(".player6 .increment1").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal++;
-    $(".p6Life").text(lifeTotal);
-  });
-  $(".player1 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p1Life").html());
-    lifeTotal--;
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p1Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p1Life").css("background-color", "#A4243B");
       $(".p1Life").css("border", "1px solid #eae0c2");
     }
   });
-  $(".player2 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal--;
+  $(".player2Buttons").on("click", function () {
+    let lifeTotal = parseInt($(".p2Life").text());
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p2Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p2Life").css("background-color", "#A4243B");
       $(".p2Life").css("border", "1px solid #eae0c2");
     }
   });
-  $(".player3 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal--;
+  $(".player3Buttons").on("click", function () {
+    let lifeTotal = parseInt($(".p3Life").text());
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p3Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p3Life").css("background-color", "#A4243B");
       $(".p3Life").css("border", "1px solid #eae0c2");
     }
   });
-  $(".player4 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal--;
+  $(".player4Buttons").on("click", function () {
+    let lifeTotal = parseInt($(".p4Life").text());
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p4Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p4Life").css("background-color", "#A4243B");
       $(".p4Life").css("border", "1px solid #eae0c2");
     }
   });
-  $(".player5 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal--;
+  $(".player5Buttons").on("click", function () {
+    let lifeTotal = parseInt($(".p5Life").text());
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p5Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p5Life").css("background-color", "#A4243B");
       $(".p5Life").css("border", "1px solid #eae0c2");
     }
   });
-  $(".player6 .decrement1").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal--;
-    $(".p6Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p6Life").css("background-color", "#A4243B");
-      $(".p6Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player1 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p1Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p1Life").text(lifeTotal);
-  });
-  $(".player2 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p2Life").text(lifeTotal);
-  });
-  $(".player3 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p3Life").text(lifeTotal);
-  });
-  $(".player4 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p4Life").text(lifeTotal);
-  });
-  $(".player5 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p5Life").text(lifeTotal);
-  });
-  $(".player6 .increment5").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal = lifeTotal + 5;
-    $(".p6Life").text(lifeTotal);
-  });
-  $(".player1 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p1Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p1Life").text(lifeTotal);
-    if (parseInt($(".p1Life").html()) <= 0) {
-      $(".p1Life").css("background-color", "#A4243B");
-      $(".p1Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player2 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p2Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p2Life").css("background-color", "#A4243B");
-      $(".p2Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player3 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p3Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p3Life").css("background-color", "#A4243B");
-      $(".p3Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player4 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p4Life").text(lifeTotal);
-    if (lifetotal <= 0) {
-      $(".p4Life").css("background-color", "#A4243B");
-      $(".p4Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player5 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p5Life").text(lifeTotal);
-    if (lifetotal <= 0) {
-      $(".p5Life").css("background-color", "#A4243B");
-      $(".p5Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player6 .decrement5").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal = lifeTotal - 5;
-    $(".p6Life").text(lifeTotal);
-    if (lifetotal <= 0) {
-      $(".p6Life").css("background-color", "#A4243B");
-      $(".p6Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player1 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p1Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p1Life").text(lifeTotal);
-  });
-  $(".player2 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p2Life").text(lifeTotal);
-  });
-  $(".player3 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p3Life").text(lifeTotal);
-  });
-  $(".player4 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p4Life").text(lifeTotal);
-  });
-  $(".player5 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p5Life").text(lifeTotal);
-  });
-  $(".player6 .increment10").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal = lifeTotal + 10;
-    $(".p6Life").text(lifeTotal);
-  });
-  $(".player1 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p1Life").html());
-    lifeTotal = lifeTotal - 10;
-    $(".p1Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p1Life").css("background-color", "#A4243B");
-      $(".p1Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player2 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p2Life").html());
-    lifeTotal = lifeTotal - 10;
-    $(".p2Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p2Life").css("background-color", "#A4243B");
-      $(".p2Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player3 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p3Life").html());
-    lifeTotal = lifeTotal - 10;
-    $(".p3Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p3Life").css("background-color", "#A4243B");
-      $(".p3Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player4 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p4Life").html());
-    lifeTotal = lifeTotal - 10;
-    $(".p4Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p4Life").css("background-color", "#A4243B");
-      $(".p4Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player5 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p5Life").html());
-    lifeTotal = lifeTotal - 10;
-    $(".p5Life").text(lifeTotal);
-    if (lifeTotal <= 0) {
-      $(".p5Life").css("background-color", "#A4243B");
-      $(".p5Life").css("border", "1px solid #eae0c2");
-    }
-  });
-  $(".player6 .decrement10").on("click", function () {
-    let lifeTotal = parseInt($(".p6Life").html());
-    lifeTotal = lifeTotal - 10;
+  $(".player6Buttons").on("click", function () {
+    let lifeTotal = parseInt($(".p6Life").text());
+    lifeTotal = lifeTotal + parseInt(this.value);
     $(".p6Life").text(lifeTotal);
     if (lifeTotal <= 0) {
       $(".p6Life").css("background-color", "#A4243B");
@@ -262,65 +67,92 @@ trackerApp.displaySetter = () => {
       if (e.which === 32) return false;
     },
   });
+  // collect user inpput
   $("form").on("submit", function (e) {
     e.preventDefault();
     playerNumber = parseInt($("#playerNumber").val());
-    console.log("trackerApp.displaySetter -> playerNumber", playerNumber);
     const startingLife = parseInt($("#startingLife").val());
-    console.log("trackerApp.displaySetter -> startingLife", startingLife);
-    if (isNaN(playerNumber)) {
-      $("#playerNumber").reset();
-    } else if (playerNumber < 0) {
-      $("#playerNumber").reset();
-    } else if (playerNumber === 0) {
+    // reset form on invalid input
+    // else show necessary player modules
+    // populate user input life total
+    if (
+      isNaN(playerNumber) ||
+      playerNumber < 0 ||
+      playerNumber > 6 ||
+      isNaN(startingLife) ||
+      startingLife < 0
+    ) {
+      $("#displayPicker").find("input:text").val("");
+    } else if (playerNumber === 1) {
+      $(".player1").toggleClass("divHider");
+    } else if (playerNumber === 2) {
+      $(".player1").toggleClass("divHider");
+      $(".player2").toggleClass("divHider");
+    } else if (playerNumber === 3) {
+      $(".player1").toggleClass("divHider");
+      $(".player2").toggleClass("divHider");
+      $(".player2").toggleClass("divHider");
+    } else if (playerNumber === 4) {
+      $(".player1").toggleClass("divHider");
+      $(".player2").toggleClass("divHider");
+      $(".player3").toggleClass("divHider");
+      $(".player4").toggleClass("divHider");
+    } else if (playerNumber === 5) {
+      $(".player1").toggleClass("divHider");
+      $(".player2").toggleClass("divHider");
+      $(".player3").toggleClass("divHider");
+      $(".player4").toggleClass("divHider");
+      $(".player5").toggleClass("divHider");
+    } else if (playerNumber === 6) {
       $(".player1").toggleClass("divHider");
       $(".player2").toggleClass("divHider");
       $(".player3").toggleClass("divHider");
       $(".player4").toggleClass("divHider");
       $(".player5").toggleClass("divHider");
       $(".player6").toggleClass("divHider");
-    } else if (playerNumber === 1) {
-      $(".player2").toggleClass("divHider");
-      $(".player3").toggleClass("divHider");
-      $(".player4").toggleClass("divHider");
-      $(".player5").toggleClass("divHider");
-      $(".player6").toggleClass("divHider");
-    } else if (playerNumber === 2) {
-      $(".player3").toggleClass("divHider");
-      $(".player4").toggleClass("divHider");
-      $(".player5").toggleClass("divHider");
-      $(".player6").toggleClass("divHider");
-    } else if (playerNumber === 3) {
-      $(".player4").toggleClass("divHider");
-      $(".player5").toggleClass("divHider");
-      $(".player6").toggleClass("divHider");
-    } else if (playerNumber === 4) {
-      $(".player5").toggleClass("divHider");
-      $(".player6").toggleClass("divHider");
-    } else if (playerNumber === 5) {
-      $(".player6").toggleClass("divHider");
     }
-    if (isNaN(startingLife)) {
-      $("#startingLife").reset();
-    } else {
-      $(".p1Life").html(startingLife);
-      $(".p2Life").html(startingLife);
-      $(".p3Life").html(startingLife);
-      $(".p4Life").html(startingLife);
+    // change grid layout for over 4 players to maintain 2 rows, reflecting typical table layout
+    if (playerNumber >= 5) {
+      $(".wrapper").css("max-width", "1200px");
     }
+    $(".p1Life").html(startingLife);
+    $(".p2Life").html(startingLife);
+    $(".p3Life").html(startingLife);
+    $(".p4Life").html(startingLife);
+    $(".p5Life").html(startingLife);
+    $(".p6Life").html(startingLife);
+    // on valid input, scroll to initialized app
     if (!isNaN(playerNumber) && !isNaN(startingLife)) {
       document.querySelector("#main").scrollIntoView({
         behavior: "smooth",
       });
     }
+    $("header").slideToggle();
   });
 };
 
-trackerApp.lifeAlert = () => {};
+trackerApp.getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+trackerApp.whoGoesFirst = () => {
+  const firstPlayer = trackerApp.getRandomInt(playerNumber) + 1;
+  if (firstPlayer === 1) {
+  }
+};
+
+trackerApp.diceRoller = () => {
+  $(".d20Button").on("click", function () {
+    diceRoll = trackerApp.getRandomInt(21);
+    console.log("trackerApp.diceRoller -> diceRoll", diceRoll);
+    $(".diceResult").text(diceRoll);
+  });
+};
 
 trackerApp.init = () => {
   trackerApp.incrementDecrement();
   trackerApp.displaySetter();
+  trackerApp.diceRoller();
 };
 
 $(function () {
